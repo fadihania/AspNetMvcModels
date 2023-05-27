eddb6d388f21be9afccb156cbbca37f65467a15b
Fadi Hania <fhania@gmail.com>
2023-05-27 21:19:57 +0300

    Add posts controller and index view

---
+++ AspNetMvcModels/Controllers/PostsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcModels.Controllers;
+public class PostsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
