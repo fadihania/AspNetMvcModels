c6b8b3a44fda8a9793652abb374dab1615b6f3fc
Fadi Hania <fhania@gmail.com>
2023-05-27 21:19:04 +0300

    Add post model and sample data to use

---
+++ AspNetMvcModels/Data.cs
@@ -0,0 +1,14 @@
+﻿using AspNetMvcModels.Models;
+
+namespace AspNetMvcModels;
+
+public class Data
+{
+    public static List<Post> Posts = new()
+    {
+        new Post { Id = 1, Title = "Post 1", Body = "Post 1 content", Author = "Ahmad Ali" },
+        new Post { Id = 2, Title = "Post 2", Body = "Post 2 content", Author = "Saeed Mahmoud" },
+        new Post { Id = 3, Title = "Post 3", Body = "Post 3 content", Author = "Osama Masoud" },
+        new Post { Id = 4, Title = "Post 4", Body = "Post 4 content", Author = "Sami Suliman" }
+    };
+}
---
+++ AspNetMvcModels/Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetMvcModels.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public string Author { get; set; }
+}
